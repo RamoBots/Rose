802f698215925a8de604505f9dd8d8e81e2da579
Ramo <albearatef@gmail.com>
2024-07-20 21:36:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = environ.get("API_HASH","5a8c3d38c4d89b672062c5ff015380c7")
 API_ID1 = int(environ.get("API_ID1","24514467"))
 API_HASH1 = environ.get("API_HASH1","5a8c3d38c4d89b672062c5ff015380c7")
 sudoers = [int(x) for x in environ.get("sudoers", "").split()]
-LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1001889013916")
+LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1002064490270")
 BASE_DB = environ.get("BASE_DB","mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
 MONGO_URL = environ.get("MONGO_URL","mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
 ARQ_API_URL = environ.get("ARQ_API_URL","https://arq.hamker.in")
